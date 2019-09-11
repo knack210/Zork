90fe639516507e69717e8ba92659e4348492fa47
knack210 <nickderosa@knights.ucf.edu>
2019-09-11 13:51:39 -0400

    Input added

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -7,6 +7,24 @@ namespace Zork
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
+
+            string inputString = Console.ReadLine();
+            inputString = inputString.ToUpper();
+
+            if (inputString == "QUIT")
+            {
+                Console.WriteLine("Thank you for playing.");
+            }
+
+            else if (inputString == "LOOK")
+            {
+                Console.WriteLine("This is an open field west of a white field, with an open door. \nA rubber mat saying 'Welcome to Zork!' lies by the door.");
+            }
+
+            else
+            {
+                Console.WriteLine("Unrecognized command.");
+            }
         }
     }
 }
