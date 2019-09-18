622152f820ff1b7b2e858c4ab27ddb928e618b1a
knack210 <nickderosa@knights.ucf.edu>
2019-09-18 14:34:21 -0400

    rooms are objects

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -5,7 +5,7 @@ namespace Zork
 {
     internal class Program
     {
-		private static String CurrentRoom
+		public static Room CurrentRoom
 		{
 			get
 			{
@@ -15,7 +15,7 @@ namespace Zork
 
 		static void Main(string[] args)
         {
-
+			InititalizeRoomDescriptions();
             Console.WriteLine("Welcome to Zork!");
 
             Commands command = Commands.UNKNOWN;        
@@ -32,7 +32,7 @@ namespace Zork
 						break;
 
 					case Commands.LOOK:
-                        Console.WriteLine("A rubber mat saying 'Welcome to Zork!' lies by the door.");
+                        Console.WriteLine(CurrentRoom.Description);
                         break;
 
                     case Commands.NORTH:
@@ -77,6 +77,7 @@ namespace Zork
 
 
 				default:
+					isValidMove = false;
                     break;
             }
 
@@ -87,10 +88,25 @@ namespace Zork
 
 		private static bool IsDirection(Commands command) => Directions.Contains(command);
 
-		private static string[,] Rooms = {   {"Rocky Trail", "South of House", "Canyon View" },
-                                            { "Forest", "West of House", "Behind House" },
-                                            {"Dense Woods", "North of House", "Clearing"}
+		private static readonly Room[,] Rooms = {   {new Room("Rocky Trail"), new Room("South of House"), new Room("Canyon View") },
+                                            { new Room("Forest"), new Room("West of House"), new Room("Behind House") },
+                                            { new Room("Dense Woods"), new Room("North of House"), new Room("Clearing") }
         };
+
+		private static void InititalizeRoomDescriptions()
+		{
+			Rooms[0, 0].Description = "You are on a rock-strewn trail.";
+			Rooms[0, 1].Description = "You are facing the south side of a white house. There is no door here, and all the windows are barred.";
+			Rooms[0, 2].Description = "You are at the top of the Great Canyon on its south wall.";
+
+			Rooms[1, 0].Description = "This is a forest, with trees in all directions around you.";
+			Rooms[1, 1].Description = "This is an open field west of a white house, with a boarded front door.";
+			Rooms[1, 2].Description = "You are behind the white house. In one corner of the house there is a small window which is slightly ajar.";
+
+			Rooms[2, 0].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight.";
+			Rooms[2, 1].Description = "You are facing the north side of a white house. There is no door here, and all the windows are barred.";
+			Rooms[2, 2].Description = "You are in a clearing, with a forest surrounding you on the west and south.";
+		}
 
 		private static readonly List<Commands> Directions = new List<Commands>
 	   {
---
+++ Zork/Room.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zork
+{
+	class Room
+	{
+		public string Name { get; }
+
+		public string Description { get; set; }
+
+		public Room(string name, string description = "")
+		{
+			Name = name;
+			Description = description;
+		}
+
+		public override string ToString() => Name;		
+	}
+}
