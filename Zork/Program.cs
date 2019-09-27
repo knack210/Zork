f136e4f49809af6ffee116b174450902de7bc24e
knack210 <nickderosa@knights.ucf.edu>
2019-09-27 00:55:34 -0400

    Large World Finished, except...
    Everything works expect it doens't seem to read the directions from my Zork.json file. I'm trying to figure why, but so far no luck.

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,150 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using Newtonsoft.Json;
-
 
 namespace Zork
 {
-    internal class Program
+    class Program
     {
-		static Program()
-		{
-			RoomMap = new Dictionary<string, Room>();
-			foreach (Room room in Rooms)
-			{
-				RoomMap[room.Name] = room;
-			}
-		}
-
-		public static Room CurrentRoom
-		{
-			get
-			{
-				return Rooms[Location.Row, Location.Column];
-			}
-		}
-
-		static void Main(string[] args)
-        {			
-            Console.WriteLine("Welcome to Zork!");
-			const string defaultRoomsFilename = "Rooms.json";
-			string roomsFilename = (args.Length > 0 ? args[(int)CommandLineArguments.RoomsFilename] : defaultRoomsFilename);
-			InititalizeRoomDescriptions(roomsFilename);
-
-			Room previousRoom = null;
-			Commands command = Commands.UNKNOWN;        
-            while (command != Commands.QUIT)
-            {
-                Console.WriteLine(CurrentRoom);
-
-				if (previousRoom != CurrentRoom)
-				{
-					Console.WriteLine(CurrentRoom.Description);
-					previousRoom = CurrentRoom;
-				}
-
-				Console.Write("> ");
-                command = ToCommand(Console.ReadLine().Trim());
-				               
-                switch (command)
-                {
-					case Commands.QUIT:
-						Console.WriteLine("Thanks you for playing!");
-						break;
-
-					case Commands.LOOK:
-                        Console.WriteLine(CurrentRoom.Description);
-                        break;
-
-                    case Commands.NORTH:
-                    case Commands.SOUTH:
-                    case Commands.EAST:
-                    case Commands.WEST:
-                        if (Move(command) == false)
-                        {
-							Console.WriteLine("The way is shut!");
-                        }
-						break;
-                       
-                    default:
-                        Console.WriteLine("Unknown command.");
-                        break;
-                };
-            }
-        }
-
-        private static bool Move(Commands command)
+        static void Main(string[] args)
         {
-			Assert.IsTrue(IsDirection(command), "Invalid direction.");
+            const string defaultGameFilename = "Zork.json";
+            string gameFilename = (args.Length > 0 ? args[(int)CommandLineArguments.GameFilename] : defaultGameFilename);
 
-			bool isValidMove = true;
-            switch (command)
-            {                
-                case Commands.NORTH when Location.Row < Rooms.GetLength(0) - 1:                   
-                    Location.Row ++;                      
-                    break;
-
-                case Commands.SOUTH when Location.Row > 0:                    
-                    Location.Row --;                    
-                    break;
-
-				case Commands.EAST when Location.Column < Rooms.GetLength(1) - 1:
-					Location.Column++;
-					break;
-
-				case Commands.WEST when Location.Column > 0:
-					Location.Column--;
-					break;
-
-
-				default:
-					isValidMove = false;
-                    break;
-            }
-
-            return isValidMove;
+            Game game = Game.Load(gameFilename);
+            Console.WriteLine("Welcome to Zork!");
+            game.Run();
+            Console.WriteLine("Thank you for playing!");
         }
 
-		private static Commands ToCommand(string commandString) => Enum.TryParse<Commands>(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
-
-		private static bool IsDirection(Commands command) => Directions.Contains(command);
-
-		private static Room[,] Rooms = {   {new Room("Rocky Trail"), new Room("South of House"), new Room("Canyon View") },
-                                            { new Room("Forest"), new Room("West of House"), new Room("Behind House") },
-                                            { new Room("Dense Woods"), new Room("North of House"), new Room("Clearing") }
-        };
-
-		private static void InititalizeRoomDescriptions(string roomsFilename)
-		{
-			var rooms = JsonConvert.DeserializeObject<Room[,]>(File.ReadAllText(roomsFilename));
-			foreach (Room room in rooms)
-			{				
-				RoomMap[room.Name].Description = room.Description;
-			}
-		}
-
-		private static readonly Dictionary<string, Room> RoomMap;
-
-		private static readonly List<Commands> Directions = new List<Commands>
-		{
-			Commands.NORTH,
-			Commands.SOUTH,
-			Commands.EAST,
-			Commands.WEST
-		};
-
-		private enum Fields
-		{
-			Name = 0,
-			Description
-		}
-
-		private enum CommandLineArguments
-		{
-			RoomsFilename = 0
-		}
-
-		private static (int Row, int Column) Location = (1, 1);
+        private enum CommandLineArguments
+        {
+            GameFilename = 0
+        }
     }
 }
